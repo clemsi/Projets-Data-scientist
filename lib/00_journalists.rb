c3280993cd762dc3915e9f487ae355792b95ab4c
clemsi <122406833+clemsi@users.noreply.github.com>
2023-01-19 18:29:47 +0100

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,99 @@
+def handleTwitter
+    return ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+# Combien y a-t-il de handle dans cette array ?
+def getHandleNumber(list) #La fonction getHandleNumber prend une liste en entrée et imprime le nombre de poignées dans la liste en utilisant la propriété length de la liste.
+    puts "Nombre de handles => #{list.length}"
+    puts "------------------------------------------------\n"
+end
+
+# Quel est le handle le plus court de cette liste ?
+def getShorterHandle(list)#getShorterHandle(list) est utilisée pour trouver le descripteur le plus court (chaîne) dans une liste de descripteurs. Il prend une liste de chaînes en entrée.
+    shorterItem = nil #La fonction déclare deux variables shortItem et shortItemAccount qui sont initialement définies sur nil.
+    shorterItemAccount = nil # shortItemAccount qui sont initialement définies sur nil
+    list.each do |i| #il parcourt chaque poignée de la liste et vérifie la longueur de chaque poignée.
+        lengthI = i.length
+        if shorterItem == nil || lengthI < shorterItem #il compare la longueur de la poignée actuelle avec le shortItem et si la poignée actuelle est plus courte que la précédente ou shortItem est nil, il attribue la poignée actuelle et sa longueur à shortItemAccount et shortItem respectivement.
+            shorterItem = lengthI # il attribue la poignée actuelle et sa longueur à shortItemAccount et shortItem respectivement.
+            shorterItemAccount = i 
+        end
+    end
+    puts "Handle le plus court de la liste => #{shorterItemAccount} (#{shorterItem} caractères)" # il imprime la poignée la plus courte de la liste avec le nombre de caractères qu'elle contient 
+    puts "------------------------------------------------\n"
+end
+
+# Combien y-a-t'il de handle contenant 5 caractères
+# (le @ ne compte pas pour 1 caractère)
+def get5charNumberHandle(list)#get5charNumberHandle(list) prend une liste de descripteurs en entrée et compte le nombre de descripteurs de 5 caractères.
+    number = 0 #Il commence par initialiser un nombre variable à 0
+    list.each do |i|
+        if i.slice!(0) == "@" #Ensuite, il parcourt chaque poignée de la liste, il vérifie si la poignée commence par "@" 
+            i = i.slice!(1, i.length) #si c'est le cas, il supprime le symbole "@" 
+        end
+        if i.length == 5 #de la poignée et vérifie si la longueur de la poignée est égal à 5.
+            number += 1 #Si la longueur est égale à 5, il incrémente la variable numérique de 1. 
+        end
+    end
+    puts "Nombre de handles de 5 caractères => #{number}" #Après la boucle, il imprime le nombre de poignées de 5 caractères. 
+    puts "------------------------------------------------\n"
+end
+
+# Combien commencent par une majuscule
+# (première lettre juste après le @) ?
+def getCapitalizeNumberHandle(list) #getCapitalizeNumberHandle(list) prend une liste de descripteurs en entrée et compte le nombre de descripteurs commençant par une majuscule.
+    number = 0 #Il commence par initialiser un numéro de variable à 0.
+    list.each do |i|
+        if i.slice!(0) == "@" # il parcourt chaque poignée de la liste, il vérifie si la poignée commence par "@"
+            i = i.slice!(1, i.length)# il supprime le symbole "@" de la poignée
+        end
+        if i[0] =~ /[A-Z]/ #vérifie si le premier caractère du handle est une lettre majuscule en utilisant l'expression régulière /[A-Z]/ 
+            number += 1 #Si le premier caractère est une lettre majuscule, il incrémente la variable numérique de 1.
+        end
+    end
+    puts "Nombre de handles commençants avec une majuscule => #{number}" #il imprime le nombre de poignées commençant par une lettre majuscule.
+    puts "------------------------------------------------\n"
+end
+
+# Trie par ordre alphabétique.
+def getAlphabeticalSortHandle(list) #getAlphabeticalSortHandle(list) prend une liste de descripteurs en entrée et trie les descripteurs par ordre alphabétique.
+    puts "Tri par ordre alphabétique :"
+    puts list = list.sort # il utilise la méthode sort sur la liste pour trier les éléments par ordre alphabétique
+    puts "------------------------------------------------\n"
+end
+
+# Trie croissant par taille des handle
+def getSizeSortHandle(list) #getSizeSortHandle(list) prend une liste de descripteurs en entrée et trie les descripteurs par taille dans l'ordre croissant.
+    puts "Tri par ordre croissant par taille :"
+    puts list = list.sort_by! { |n| n.size } #il utilise le sort_by! méthode sur la liste avec un bloc utilisant { |n| n.size } qui trie les poignées par taille. 
+    puts "------------------------------------------------\n"
+end
+
+# Quelle est la position dans l'array de la personne @epenser ?
+def getValueArrayPositionHandle(list, string) #getValueArrayPositionHandle(list, string) prend une liste de descripteurs et une chaîne en entrée.
+    filterList = list.select { |n| n == string } #Il utilise la méthode select pour créer une nouvelle liste appelée filterList qui contient tous les éléments de la liste d'origine qui sont égaux à la chaîne d'entrée
+    positionList = list.find_index { |n| n == string } # La fonction utilise ensuite la méthode find_index pour trouver la position de la chaîne dans la liste d'origine. 
+    puts "Position de #{filterList} dans l'array : #{positionList}" #Il imprime ensuite la position de la chaîne dans la liste d'origine et une ligne de séparation à la fin.Il convient de noter que la variable filterList ne contiendra toujours qu'un seul élément car la chaîne d'entrée doit être unique dans le tableau, de sorte que la filterList ne contiendra qu'un seul élément.
+    puts "------------------------------------------------\n"
+end
+
+# Répartition par taille de ces derniers (avec 1, 2, 3... caractères)
+def getCharactersSizeSortHandle(list) #getCharactersSizeSortHandle(list) prend une liste de descripteurs en entrée et trie les descripteurs par nombre de caractères dans l'ordre croissant. 
+    sortList = list.sort_by! { |n| n.length } #il imprime la liste triée des poignées, il utilise le sort_by! méthode sur la liste avec un bloc utilisant { |n| n.length } qui trie les poignées par le nombre de caractères dans chaque poignée.
+    puts "Tri par taille :"
+    puts sortList
+    puts "------------------------------------------------\n"
+end
+
+def perform
+    getHandleNumber(handleTwitter)
+    getShorterHandle(handleTwitter)
+    get5charNumberHandle(handleTwitter)
+    getCapitalizeNumberHandle(handleTwitter)
+    getAlphabeticalSortHandle(handleTwitter)
+    getSizeSortHandle(handleTwitter)
+    getValueArrayPositionHandle(handleTwitter, "@epenser")
+    getCharactersSizeSortHandle(handleTwitter)
+end
+
+perform
